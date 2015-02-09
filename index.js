1cef1e948e0b8c789e25a50f7635273c4398ed65
Brandon Burns <brandon10688@gmail.com>
2015-02-09 12:20:12 -0800

    final files

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-'use strict'
+'use strict';
 
 var fs = require('fs');
-var bitmap = fs.readFileSync('test.bmp');
-console.log(bitmap);
+var oldFile = process.argv[2].toString();
+var bitmap = fs.readFileSync(oldFile);
 
-var bitmapObject = {};
+var displayBMPProps = require('./lib/readFile');
+var randomize = require('./lib/randomize');
+var greyScale = require('./lib/greyScale');
 
-bitmapObject.type = bitmap.toString('utf-8', 0, 2);
-bitmapObject.size = bitmap.readUInt32LE(2);
-bitmapObject.startOfPixels = bitmap.readUInt32LE(10);
-bitmapObject.headerSize = bitmap.readUInt32LE(14);
-bitmapObject.width = bitmap.readUInt32LE(18);
-bitmapObject.height = bitmap.readUInt32LE(22);
-bitmapObject.colorDepth = bitmap.readUInt16LE(28);
-bitmapObject.paletteSize = bitmap.readUInt32LE(46);
+displayBMPProps(bitmap);
 
-
-
-bitmap[54] = 0;
-bitmap[55] = 100;
-bitmap[56] = 240;
-bitmap[57] = 255;
-
-console.log(bitmap[54]);
-
-// for (var i = 54; i < 1078; i++) {
-// 	bitmap[i] = (Math.random() * 255);
-// };
-
-// for (var i = 1078; i < 11078; i++) {
-// 	bitmap[i] = (Math.random() * 255);
-// };
-
-console.dir(bitmapObject);
-
-fs.writeFile('test2.bmp', bitmap, function(err, success) {
-  if (err) throw err;
-  console.log('Success!');
-});
-
-
-
+greyScale(bitmap, oldFile);
